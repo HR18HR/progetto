bb53efe82584ea24ad578680722d597947bdb59f
HR18HR <hamza.rofi@gmail.com>
2026-03-22 11:47:23 +0100

    commento

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy()
 
+#rotta da cancellare
 @app.route("/")
 def index():
-    return "Hello World!"
-
+    return "prova" 
 
 if __name__ == "__main__":
     app.run(debug=True)
